d2fd8bece8adf3f326c4916c86109c044baed292
isuri97 <you@example.com>
2023-10-04 22:39:24 +0530

    add test file

--- pretraining/test_file.py
+++ pretraining/test_file.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 
-text = "People were taken into the <mask> concentration camp."
+text = "People were taken into the <mask> camp."
 
 mask_filler = pipeline("fill-mask", "/tmp/test-mlm")
 print(mask_filler(text, top_k=3))
