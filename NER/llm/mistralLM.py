5bbca7709d4d5082d7698dbce4182bd8f658d63b
isuri97 <you@example.com>
2023-11-01 17:54:05 +0530

    add mistralllm file

--- NER/llm/mistralLM.py
+++ NER/llm/mistralLM.py
@@ -3,22 +3,30 @@ import transformers
 import torch
 import pandas as pd
 
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_use_double_quant=True,
-)
+# bnb_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_use_double_quant=True,
+# )
 
 model_name = 'mistralai/Mistral-7B-Instruct-v0.1'
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModelForCausalLM.from_pretrained(
+#         model_name,
+#         load_in_4bit=True,
+#         quantization_config=bnb_config,
+#         torch_dtype=torch.bfloat16,
+#         device_map="auto",
+#         trust_remote_code=True,
+#     )
+
 model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        load_in_4bit=True,
-        quantization_config=bnb_config,
         torch_dtype=torch.bfloat16,
         device_map="auto",
         trust_remote_code=True,
+        offload_folder="offload"
     )
 
 pipe = pipeline(
