a3f82c3a4c984d76a7ad4eeb92f0dbfd4d2b9c3a
isuri97 <you@example.com>
2023-11-01 19:36:00 +0530

    add mistralllm file

--- NER/llm/mistralLM.py
+++ NER/llm/mistralLM.py
@@ -3,38 +3,39 @@ import transformers
 import torch
 import pandas as pd
 
-# bnb_config = BitsAndBytesConfig(
-#     load_in_4bit=True,
-#     bnb_4bit_quant_type="nf4",
-#     bnb_4bit_use_double_quant=True,
-# )
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+)
 
 model_name = 'mistralai/Mistral-7B-Instruct-v0.1'
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-# model = AutoModelForCausalLM.from_pretrained(
-#         model_name,
-#         load_in_4bit=True,
-#         quantization_config=bnb_config,
-#         torch_dtype=torch.bfloat16,
-#         device_map="auto",
-#         trust_remote_code=True,
-#     )
-
 model = AutoModelForCausalLM.from_pretrained(
         model_name,
+        quantization_config=bnb_config,
         torch_dtype=torch.bfloat16,
         device_map="auto",
         max_memory={0:'20GIB'},
         trust_remote_code=True,
     )
 
+# model = AutoModelForCausalLM.from_pretrained(
+#         model_name,
+#         torch_dtype=torch.bfloat16,
+#         device_map="auto",
+#         max_memory={0:'20GIB'},
+#         trust_remote_code=True,
+#     )
+
 pipe = pipeline(
     "text-generation",
     model=model,
     tokenizer = tokenizer,
     torch_dtype=torch.bfloat16,
-    device_map="auto"
+    device_map="auto",
+    max_memory={1:'20GIB'},
 )
 
 
@@ -44,10 +45,10 @@ data = pd.read_csv('data1.csv', sep=",")
 for index, row in data.iterrows():
   text = row['tags']
 
-  prompt = f"""Return a list of named entities in the text.
+  prompt = f"""Return a only list of named entities in the text. No need of additional information. Provide only 
           Text: ```{text}```
           Named entities:
-          Dont generate other useless information.
+          
 """
   sequences = pipe(
       prompt,
