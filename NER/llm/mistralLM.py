57f83c8be1f12314c0692fd4336c74a024183a65
isuri97 <you@example.com>
2023-11-03 03:06:46 +0530

    add mistralllm file

--- NER/llm/mistralLM.py
+++ NER/llm/mistralLM.py
@@ -1,3 +1,67 @@
+# from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
+# import transformers
+# import torch
+# import pandas as pd
+#
+# bnb_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_use_double_quant=True,
+# )
+#
+# model_name = 'mistralai/Mistral-7B-Instruct-v0.1'
+#
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModelForCausalLM.from_pretrained(
+#         model_name,
+#         quantization_config=bnb_config,
+#         torch_dtype=torch.bfloat16,
+#         device_map="auto",
+#         max_memory={0:'20GIB'},
+#         trust_remote_code=True,
+#     )
+#
+# # model = AutoModelForCausalLM.from_pretrained(
+# #         model_name,
+# #         torch_dtype=torch.bfloat16,
+# #         device_map="auto",
+# #         max_memory={0:'20GIB'},
+# #         trust_remote_code=True,
+# #     )
+#
+# pipe = pipeline(
+#     "text-generation",
+#     model=model,
+#     tokenizer = tokenizer,
+#     torch_dtype=torch.bfloat16,
+#     device_map="auto",
+#     max_memory={1:'20GIB'},
+# )
+#
+#
+# data = pd.read_csv('data1.csv', sep=",")
+#
+#
+# for index, row in data.iterrows():
+#   text = row['tags']
+#
+#   prompt = f"""Return a only list of named entities in the text. No need of additional information. Provide only
+#           Text: ```{text}```
+#           Named entities:
+#
+# """
+#   sequences = pipe(
+#       prompt,
+#       do_sample=True,
+#       top_k=10,
+#       num_return_sequences=1,
+#       eos_token_id=tokenizer.eos_token_id,
+#       max_length=200,
+#   )
+#
+# for seq in sequences:
+#     print(f"Result: {seq['generated_text']}")
+
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
 import transformers
 import torch
@@ -14,28 +78,19 @@ model_name = 'mistralai/Mistral-7B-Instruct-v0.1'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
         model_name,
+        load_in_4bit=True,
         quantization_config=bnb_config,
         torch_dtype=torch.bfloat16,
         device_map="auto",
-        max_memory={0:'20GIB'},
         trust_remote_code=True,
     )
-
-# model = AutoModelForCausalLM.from_pretrained(
-#         model_name,
-#         torch_dtype=torch.bfloat16,
-#         device_map="auto",
-#         max_memory={0:'20GIB'},
-#         trust_remote_code=True,
-#     )
 
 pipe = pipeline(
     "text-generation",
     model=model,
     tokenizer = tokenizer,
     torch_dtype=torch.bfloat16,
-    device_map="auto",
-    max_memory={1:'20GIB'},
+        device_map="auto"
 )
 
 
@@ -44,11 +99,16 @@ data = pd.read_csv('data1.csv', sep=",")
 
 for index, row in data.iterrows():
   text = row['tags']
+  # prompt = f"""
+  # Think you are a historian and you are supposed to find named entities and relationships in holocaust text.
+  # - First identify the named entities with their named entity tags of the given text delimited by ```
+  # - Print the named entities only
+  # holocaust text : ```{text}```"""
 
-  prompt = f"""Return a only list of named entities in the text. No need of additional information. Provide only 
+  prompt = f"""Return a list of named entities in the text.
           Text: ```{text}```
           Named entities:
-          
+          Dont generate other useless information.
 """
   sequences = pipe(
       prompt,
