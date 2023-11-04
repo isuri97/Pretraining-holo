fdfe47de4e9776cf7230ccde4bbed798ca925019
isuri97 <you@example.com>
2023-11-04 05:48:30 +0530

    change runfile

--- NER/experiments/model/run_file.py
+++ NER/experiments/model/run_file.py
@@ -21,7 +21,7 @@ arguments = parser.parse_args()
 # val_df = pd.read_csv('val.csv', sep='\t', usecols=['words','labels','sentence_id'])
 
 train_df = pd.read_csv(arguments.train, sep=',', usecols=['words','labels','sentence_id'])
-test_df = pd.read_csv(arguments.test, sep=',', usecols=['words','labels','sentence_id'])
+test_df = pd.read_csv(arguments.test, sep='\t', usecols=['words','labels','sentence_id'])
 
 model_args = NERArgs()
 
